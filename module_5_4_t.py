c12ce2deaf433afc8fe1d3b064eaec88ca656826
Azam4t17 <AyzekMendes@gmail.com>
2024-10-08 15:18:54 +0500

    Add files via upload

---
+++ module_5_4_t.py
@@ -0,0 +1,28 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        if args:
+            cls.houses_history.append(args[0])
+        if kwargs:
+            cls.houses_history.append(kwargs)
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        # House.houses_history.append((self.name, self.number_of_floors))
+
+    def __del__(self):
+        print(f'{self.name} снесен, но останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+del h2
+del h3
+print(House.houses_history)
